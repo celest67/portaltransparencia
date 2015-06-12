d65798964f6b705e624757af30211e6e31082104
driveros <driveros@gmail.com>
2015-06-12 18:07:55 -0300

    Adding menu

--- pdt-theme/js/jquery.menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#showLeftPush').click(function(){
-		console.log('asdad');
-		$(this).toggleClass('active');
-		$('body').toggleClass('cbp-spmenu-push-toright');
-		$('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
-	});
-});
