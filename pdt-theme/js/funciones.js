9c0213da37ae8b62261f69d07b928f5b99da02ef
driveros <driveros@gmail.com>
2015-06-14 13:26:00 -0300

    autocomplete

--- pdt-theme/js/funciones.js
+++ pdt-theme/js/funciones.js
@@ -45,4 +45,50 @@ $(document).ready(function () {
         $('body').toggleClass('cbp-spmenu-push-toright');
         e.preventDefault();
     });
+
+    $jq.widget( "custom.catcomplete", $jq.ui.autocomplete, {
+        _create: function() {
+          this._super();
+          this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
+        },
+        _renderMenu: function( ul, items ) {
+          var that = this,
+            currentCategory = "";
+          $jq.each( items, function( index, item ) {
+            var li;
+            if ( item.category != currentCategory ) {
+              ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+              currentCategory = item.category;
+            }
+            li = that._renderItemData( ul, item );
+            if ( item.category ) {
+              li.attr( "aria-label", item.category + " : " + item.label );
+            }
+          });
+        }
+    });
+
+    var autoCompleteValues = [];
+    $jq.each(
+        $jq('#menu-accordion>h4'), 
+        function(){
+            var newCategory = $jq.trim($jq(this).text());
+            var newLinks = $jq(this).next('div').find('a');
+            
+            $jq.each(
+                newLinks,
+                function(){                 
+                    autoCompleteValues.push({
+                        "label": $jq.trim($jq(this).text()),
+                        "category": newCategory
+                    });
+                }
+            );
+        }
+    );
+
+    $jq("#menu-search-autocomplete").catcomplete({
+      delay: 0,
+      source: autoCompleteValues
+    });
 });
